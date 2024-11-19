013ef10319e2665b3a66cef157245f0f840ccf0d
hannamkrt <103834678+hannamkrt@users.noreply.github.com>
2024-11-19 16:17:11 +0100

    add terraform config, gitignore file and update readme

---
+++ tf/main.tf
@@ -0,0 +1,32 @@
+# Resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
+  node_resource_group = var.node_resource_group
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = "Standard_A2_v2"
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet"
+  }
+}
---
+++ tf/outputs.tf
@@ -0,0 +1,19 @@
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "endpoint" {
+  value = azurerm_kubernetes_cluster.aks.endpoint
+}
