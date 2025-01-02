17306b40b23b1dbf8124277f6272b5f07797bb67
hannamkrt <103834678+hannamkrt@users.noreply.github.com>
2025-01-02 20:13:28 +0100

    remove backend.tf file and update deploy.yml for Kubernetes CI/CD

--- tf/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "CG-tracks"
-    storage_account_name  = "backendtfplatformtrack"
-    container_name        = "terraform-state"
-    key                    = "terraform.tfstate"
-  }
-}
